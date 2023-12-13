da61e046532e6dbdc16ac473c2c6d72e10df0fa1
DeanWilcox <145401932+DeanWilcox@users.noreply.github.com>
2023-12-13 15:27:10 -0800

    Re-upload after Mongo Reactive replacement
    Wrong Mongo dependency added on Spring, had to create a whole new project to fix error.

---
+++ demo/src/main/java/com/example/demo/Controller/RestaurantController.java
@@ -0,0 +1,37 @@
+package com.example.demo.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//localhost:8080/api/restaurants
+
+import com.example.demo.Model.Restaurant;
+import com.example.demo.Repository.RestaurantRepository;    
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    /**
+     * @param borough
+     * @return
+     */
+    @GetMapping("/{borough}")
+    public ResponseEntity<List<Restaurant>> FindRestaurantByBorough(@PathVariable(value = "borough") String borough){
+        List<Restaurant> restaurants = restaurantRepository.findByBorough(borough);
+
+        if(restaurants != null){
+            return ResponseEntity.ok(restaurants);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.example.demo.Controller.RestaurantController;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+		RestaurantController restaurantController;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+	}
+
+}
